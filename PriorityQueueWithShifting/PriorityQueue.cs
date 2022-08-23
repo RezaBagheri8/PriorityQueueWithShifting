85cc452d78c5aed734310392a4f14a8c323e0a56
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 20:07:49 +0430

    Initial Repo

---
+++ PriorityQueueWithShifting/PriorityQueue.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueueWithShifting
+{
+    public class PriorityQueue
+    {
+        public int[] queue;
+        public int size;
+        public int front;
+        public int rear;
+
+        public PriorityQueue(int sizeOfQueue)
+        {
+            size = sizeOfQueue;
+            queue = new int[sizeOfQueue];
+            front = -1;
+            rear = -1;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The queue is full!");
+            }
+            else
+            {
+                rear++;
+                queue[rear] = item;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                int maxIndex = 0;
+                int max = queue[0];
+                for (int i = front + 1; i <= rear; i++)
+                {
+                    if (max < queue[i])
+                    {
+                        max = queue[i];
+                        maxIndex = i;
+                    }
+                }
+
+                Console.WriteLine(queue[maxIndex]);
+                rear--;
+
+                int j = maxIndex;
+                for (int i = maxIndex + 1; i <= rear; i++)
+                {
+                    queue[j] = queue[i];
+                    j++;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty");
+            }
+
+            for (int i = front + 1; i <= rear; i++)
+            {
+                Console.WriteLine(queue[i]);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (rear == -1 || front == rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsFull()
+        {
+            if ((rear + 1) == size)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PriorityQueueWithShifting/Program.cs
@@ -0,0 +1,20 @@
+﻿using PriorityQueueWithShifting;
+
+var queue = new PriorityQueue(10);
+
+queue.Dequeue();
+queue.Enqueue(2);
+queue.Enqueue(5);
+queue.Enqueue(6);
+queue.Print();
+
+
+
+
+
+queue.Enqueue(90);
+
+queue.Dequeue();
+queue.Print();
+
+
